12f6029eef4dfc1d293187c91f5056fbc3f2ee3b
Dejah Cooper <Dejah.C@icloud.com>
2024-11-22 20:14:58 -0500

    added db to file

--- data.sql
+++ data.sql
@@ -1,10 +1,13 @@
-CREATE TABLE books (
-  isbn TEXT PRIMARY KEY,
-  amazon_url TEXT,
-  author TEXT,
-  language TEXT, 
-  pages INTEGER,
-  publisher TEXT,
-  title TEXT, 
-  year INTEGER
+\c bookstore
+CREATE TABLE books(
+  isbn text PRIMARY KEY,
+  amazon_url text,
+  author text,
+  language TEXT
+,
+  pages integer,
+  publisher text,
+  title text,
+  year integer
 );
+
